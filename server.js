9695c4f63a54a018b23af0981dbd3f0b902577b7
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-06-26 19:29:26 +0530

    added mongodb

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClint = require('mongodb').MongoClient;
+
 
 app.listen(3000, function(){
     console.log('listening on 3000');
 })
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.sendFile('C:/Users/thaku/Desktop/100-Devs/Projects/StarWars/index.html');
 })
 
 app.post('/quotes', (req,res) => {
-    console.log("Hellooooooooooooooooo")
+    console.log(req.body);
 })
 
