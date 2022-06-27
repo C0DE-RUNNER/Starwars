76201b4a8964e93e415f1859659d6faa517b0d2b
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-06-27 16:43:58 +0530

    added ejs engine

--- server.js
+++ server.js
@@ -12,13 +12,18 @@ MongoClient.connect('mongodb+srv://starwars:starwars%40123@cluster0.idlrdxn.mong
     app.use(bodyParser.urlencoded({ extended: true }))
     
     app.get('/', (req, res) => {
+        db.collection('quotes').find().toArray()
+    .then(results => {
+      console.log(results)
+    })
+    .catch(error => console.error(error))
         res.sendFile('C:/Users/thaku/Desktop/100-Devs/Projects/StarWars/index.html');
     })
     
     app.post('/quotes', (req,res) => {
         quoteCollection.insertOne(req.body)
         .then(result => {
-          console.log(result)
+          res.redirect('/')
         })
         .catch(error => console.error(error))
     })
