7a6662f182e0c72fdcd7ebb3f0570641fbfdfe88
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-06-27 18:53:23 +0530

    added send request

--- public/main.js
+++ public/main.js
@@ -19,3 +19,34 @@ update.addEventListener('click', _ => {
   app.put('/quotes', (req, res) => {
     console.log(req.body)
   })
+
+  quotesCollection.findOneAndUpdate(
+    { name: 'Yoda' },
+    {
+      $set: {
+        name: req.body.name,
+        quote: req.body.quote
+      }
+    },
+    {
+      upsert: true
+    }
+  )
+    .then(result => {/* ... */})
+    .catch(error => console.error(error))
+
+    app.put('/quotes', (req, res) => {
+        quotesCollection.findOneAndUpdate(/* ... */)
+          .then(result => {
+             res.json('Success')
+           })
+          .catch(error => console.error(error))
+      })
+
+      fetch({ /* request */ })
+  .then(res => {
+    if (res.ok) return res.json()
+  })
+  .then(response => {
+    console.log(response)
+  })
