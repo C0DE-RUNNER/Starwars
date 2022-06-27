dffde1f3af9971a880b05b496d0f14c0bae12ad0
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-06-27 15:30:54 +0530

    1st database entry

--- server.js
+++ server.js
@@ -7,21 +7,27 @@ MongoClient.connect('mongodb+srv://starwars:starwars%40123@cluster0.idlrdxn.mong
 .then(client => {
     console.log('Connected to Database');
     const db = client.db('starwars-quotes');
+    const quoteCollection = db.collection('quotes');
+    
+    app.use(bodyParser.urlencoded({ extended: true }))
+    
+    app.get('/', (req, res) => {
+        res.sendFile('C:/Users/thaku/Desktop/100-Devs/Projects/StarWars/index.html');
+    })
+    
+    app.post('/quotes', (req,res) => {
+        quoteCollection.insertOne(req.body)
+        .then(result => {
+          console.log(result)
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.listen(3000, function(){
+        console.log('listening on 3000');
+    })
+
   })
 .catch(console.error);
 
-
-app.listen(3000, function(){
-    console.log('listening on 3000');
-})
-
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/', (req, res) => {
-    res.sendFile('C:/Users/thaku/Desktop/100-Devs/Projects/StarWars/index.html');
-})
-
-app.post('/quotes', (req,res) => {
-    console.log(req.body);
-})
 
